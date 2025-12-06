d0336e38bea993a90a34085600aa9447f530238f
Kelly Maass <kmaass77627@gmail.com>
2025-12-06 15:26:24 -0600

    new

--- index.js
+++ index.js
@@ -2,14 +2,18 @@
 // import "bootstrap/dist/js/bootstrap.bundle.min.js";
 
 document.addEventListener("DOMContentLoaded", () => {
-  let shopbtn = document.getElementsByClassName("fas fa-shopping-cart")
+  let shopbtn = document.getElementById("cart-btn")
   shopbtn.addEventListener("click", (e) => toggleCart(e))
+
+  let cartCanvas = document.getElementById("cartCanvas")
+
 
 
 
 fetch("http://localhost:3000/cheeses")
   .then(res => res.json())
   .then(cheeses => {
+    console.log(cheeses)
     cheeses.forEach(cheese => renderCheeseCard(cheese));
   });
 
@@ -37,6 +41,18 @@ fetch("http://localhost:3000/cheeses")
     function addToCart(e, cheese) {
       console.log(e)
       console.log(cheese)
+      let cartCard = document.createElement("div")
+      cartCard.className = "cart-card"
+      cartCard.innerHTML = `
+      <img src="${cheese.image}" class="cart-img" />
+      <h3 class="cart-name">${cheese.name}</h3>
+      <p class="cart-description">${cheese.description}</p>
+      <p class="cart-price">$${cheese.price}</p>
+      `;
+
+
+
+      cartCanvas.appendChild(cartCard)
     }
 
     function toggleCart(e) {
